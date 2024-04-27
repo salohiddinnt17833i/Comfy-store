cd31522018324e4b7c20b8f4d85404b1305e7a9e
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2024-04-27 13:54:35 +0500

    Detailes end

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -5,6 +5,8 @@ function Details() {
   const params = useParams()
   const navigate = useNavigate()
   const [data, setData] = useState(null)
+  const [selectedColor, setSelectedColor] = useState('')
+  const [count, setCount] = useState(1)
 
   useEffect(() => {
     if (Number(params.id)) {
@@ -12,6 +14,7 @@ function Details() {
         .then(res => res.json())
         .then(data => {
           setData(data.data)
+          setSelectedColor(data.data.attributes.colors[0])
         })
         .catch(err => {
           console.log(err);
@@ -30,23 +33,57 @@ function Details() {
         </ul>
       </div>
 
-      {
-        data?.id && <>
-          <div className="flex gap-16">
-            <div className="w-96">
-              <img className="w-96 h-96 object-cover rounded-lg lg:w-full" src={data.attributes.image} alt="" />
-            </div>
-            <div className="w-[512px]">
-              <h2 className="capitalize text-3xl font-bold">{data.attributes.title}</h2>
-              <h3 className="text-xl text-neutral-content font-bold mt-2">{data.attributes.company}</h3>
-              <h3 class="mt-3 text-xl">$ {data.attributes.price / 100}</h3>
-              <p className="mt-6 leading-8">{data.attributes.description}</p>
-              <div className="form">
+      <div>
+        {
+          data?.id && <>
+            <div className="flex gap-16">
+              <div className="w-96">
+                <img className="w-96 h-96 object-cover rounded-lg lg:w-full" src={data.attributes.image} alt="" />
+              </div>
+              <div className="w-[512px]">
+                <h2 className="capitalize text-3xl font-bold">{data.attributes.title}</h2>
+                <h3 className="text-xl text-neutral-content font-bold mt-2">{data.attributes.company}</h3>
+                <h3 class="mt-3 text-xl">$ {data.attributes.price / 100}</h3>
+                <p className="mt-6 leading-8">{data.attributes.description}</p>
+                <div className="form  mt-3">
+                  <p className="font-bold block my-3">Colors</p>
+                  <div className="flex items-center gap-2">
+                    {
+                      data.attributes.colors.map((color, index) => {
+                        return (
+                          <span style={{
+                            backgroundColor: color,
+                            border: color == selectedColor ? '1px solid black' : 'none'
+                          }} className="w-5 h-5 rounded-full block cursor-pointer" onClick={() => setSelectedColor(color)}></span>
+                        )
+                      })
+                    }
+
+                  </div>
+
+                </div>
+                <div className="flex flex-col mt-7 gap-3">
+                  <label htmlFor="select">Amount</label>
+                  <select name="select" id="select"
+                    className="select select-bordered w-full max-w-xs"
+                    value={count}
+                    onChange={(e) => {
+                      setCount(e.target.value)
+                    }}
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                </div>
+                <button className="btn btn-primary mt-8 mb-20 uppercase ">ADD TO BAG</button>
               </div>
             </div>
-          </div>
-        </>
-      }
+          </>
+        }
+      </div>
 
     </div>
   )
